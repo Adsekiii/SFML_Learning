c80cae3964e966a48a02e209a2cddc8120055d63
Adsekiii <adrianzurawski@proton.me>
2024-10-25 15:34:08 +0200

    I've done some funny stuff with controlling a rectangle

--- SfmlTutorial/main.cpp
+++ SfmlTutorial/main.cpp
@@ -6,40 +6,52 @@
 int main() {
 
 	sf::VideoMode windowSize(800, 600);
-	sf::String title = "TextInput";
-	std::string text = "";
+	sf::String title = "Titel";
+	sf::ContextSettings settings;
+	sf::RenderWindow window(windowSize, title, sf::Style::Default, settings);
 
-	sf::Window window(windowSize, title);
+	window.setFramerateLimit(240);
 
-	window.setVerticalSyncEnabled(true);
+	sf::RectangleShape rect1(sf::Vector2f(50.f, 50.f));
+	rect1.setFillColor(sf::Color::Red);
+	rect1.setPosition(sf::Vector2f(375.f, 275.f));
 
+	settings.antialiasingLevel = 8;
 
-	bool wasPressed = false;
-
-	while (window.isOpen()) {
-
+	while (window.isOpen()){
 		sf::Event event;
-		while (window.pollEvent(event)) {
-
-			if (event.type == sf::Event::TextEntered)
-			{
-				if (event.text.unicode < 128)
-					text += static_cast<char>(event.text.unicode);
-			}
-
-			if (event.type == sf::Event::KeyPressed) {
-				if (event.key.scancode == sf::Keyboard::Scan::Enter) {
-					std::cout << text << std::endl;
-					text = "";
-				}
-			}
-
+		while (window.pollEvent(event))
+		{
 			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
 
 		}
 
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::W)) {
+			sf::Vector2f currPos = rect1.getPosition();
+			currPos.y -= 1.f;
+			rect1.setPosition(sf::Vector2f(currPos.x,currPos.y));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::S)) {
+			sf::Vector2f currPos = rect1.getPosition();
+			currPos.y += 1.f;
+			rect1.setPosition(sf::Vector2f(currPos.x, currPos.y));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::A)) {
+			sf::Vector2f currPos = rect1.getPosition();
+			currPos.x -= 1.f;
+			rect1.setPosition(sf::Vector2f(currPos.x, currPos.y));
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scan::D)) {
+			sf::Vector2f currPos = rect1.getPosition();
+			currPos.x += 1.f;
+			rect1.setPosition(sf::Vector2f(currPos.x, currPos.y));
+		}
+
+		window.clear(sf::Color::Black);
+		window.draw(rect1);
+		window.display();
 	}
 
 	return 0;
