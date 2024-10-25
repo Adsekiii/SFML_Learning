63afac3243f325df100867ea0e93eec2cc699aa9
Adsekiii <adrianzurawski@proton.me>
2024-10-25 12:17:59 +0200

    Made code that gets input in window and after pressing enter outputs it into console

--- SfmlTutorial/main.cpp
+++ SfmlTutorial/main.cpp
@@ -1,38 +1,48 @@
-﻿#include <SFML/System.hpp>
-#include <SFML/Graphics.hpp>
+﻿#include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
 
-int main()
-{
-    sf::VideoMode windowSize(800, 600);
-    sf::String title = "Moje okno";
+int main() {
 
-    sf::Window window(windowSize,title);
-    window.setFramerateLimit(120);
+	sf::VideoMode windowSize(800, 600);
+	sf::String title = "TextInput";
+	std::string text = "";
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            switch (event.type)
-            {
-            case sf::Event::LostFocus :
-                title = "LostFocus";
-                window.setTitle(title);
-                break;
+	sf::Window window(windowSize, title);
 
-            case sf::Event::GainedFocus :
-                title = "GainedFocus";
-                window.setTitle(title);
-                break;
+	window.setVerticalSyncEnabled(true);
 
-            case sf::Event::Closed:
-                window.close();
-                break;
-            default:
-                break;
-            }
-        }
-    }
 
-    return 0;
+	bool wasPressed = false;
+
+	while (window.isOpen()) {
+
+		sf::Event event;
+		while (window.pollEvent(event)) {
+
+			if (event.type == sf::Event::TextEntered)
+			{
+				if (event.text.unicode < 128)
+					text += static_cast<char>(event.text.unicode);
+			}
+
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.scancode == sf::Keyboard::Scan::Enter) {
+					std::cout << text << std::endl;
+					text = "";
+				}
+			}
+
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+
+		}
+
+	}
+
+	return 0;
 }
+
+
